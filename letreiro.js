95a7bf1aac6d72b54345d7d1357bb84723e70fe1
mayron <mayroncb@gmail.com>
2019-07-23 20:53:24 -0300

    letreiro - aula 5

---
+++ letreiro.js
@@ -0,0 +1,30 @@
+
+var evento = [];
+
+function muda()
+{
+    var letreiro = document.getElementById("letreiro");
+
+    var texto = letreiro.innerHTML;
+    var letra = texto.charAt(0);
+
+    texto += letra;
+
+    letreiro.innerHTML = texto.substr(1);
+}
+
+function liga()
+{
+    // callback -> função que responde a um evento.
+    evento.push(setInterval(muda, 200));
+}
+
+function desliga()
+{
+    evento.forEach(function(val, idx){
+        clearInterval(val);
+    });
+}
+
+console.log("liga");
+liga();
