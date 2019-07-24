735c2ef353084d37cbafc08b293b11eac02c0bb8
mayron <mayroncb@gmail.com>
2019-07-24 20:09:18 -0300

    jquery aula 6

---
+++ jquery.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+        $(".btn").click(function(){
+            $('#texto').css("background-color", "yellow");
+        });
+
+        $("p").click(function(){
+            $(this).css("background-color", "yellow");
+        });
+});
+
